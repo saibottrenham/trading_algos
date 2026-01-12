409614c6544225766625e19c0447cbe4859240d4
Tobias Mahnert <tobiasmahnert@Tobiass-Laptop.local>
2026-01-12 13:26:22 +1100

    comment enhancement

--- trading_algos/trail_my_trade.py
+++ trading_algos/trail_my_trade.py
@@ -273,7 +273,7 @@ def main():
                                 log_event("INSUFFICIENT_MARGIN_SKIP_OPEN", ticket=ticket, required=margin_req)
                             else:
                                 tp_to_set = ap['target'] if ap['target'] is not None else 0.0
-                                comment = f"Chain from #{ticket}"[:31]  # Truncate to MT5 limit
+                                comment = f"from #{ticket}"[:31]  # Truncate to MT5 limit
                                 new_ticket = Broker.open_market_position(ap['symbol'], action, ap['volume'], tp=tp_to_set, comment=comment)
                                 if new_ticket:
                                     chained_positions.add(new_ticket)
